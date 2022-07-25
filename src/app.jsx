464fa0715369f57d08a4d22723da0619cf702382
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-25 21:55:39 +0500

    e-commerce codes

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar";
+import ProductList from "./components/product-list";
+import Details from "./components/details";
+import Cart from "./components/cart";
+import Default from "./components/default";
+import "./base.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </>
+    );
+  }
+}
+
+export default App;
